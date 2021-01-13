af97d29c21fd2cd52a3393389653c91217b0dc92
gqadirdepar <gqadirdepar@gmail.com>
2021-01-13 16:35:24 +0500

    coma in included
    error occured due to comma not included in url patterns

--- home/urls.py
+++ home/urls.py
@@ -17,5 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from home import views
 urlpatterns = [
-   path( "" , views.index , name="home")
+   path( "" , views.index , name="home"),
+   path('about',views.about, name='about'),
+  # path('about' , views.about , name="about")
+   #path( "about" , views.about , name="about")
+   path( "services" , views.services , name="services"),
+   path( "contact" , views.contact , name="contact")
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render , HttpResponse
+# Create your views here.
 
 def index(request):
     return HttpResponse('This is home [page')
 
-# Create your views here.
+def about(request):
+    return HttpResponse('This is  about page')
+
+def services(request):
+    return HttpResponse('This is services page')
+
+def contact(request):
+    return HttpResponse('This is contact page')
+
+
