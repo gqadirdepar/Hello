ed13e9b7492b941e999d0cf6df127891807cb267
gqadirdepar <38527806+gqadirdepar@users.noreply.github.com>
2021-01-13 22:05:09 +0500

    Secret file information added
    secret information added

--- Hello/settings.py
+++ Hello/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 from Hello import shop_secret
+from Hello import Secret_file
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 #SECRET_KEY = '44pythjio#a!p5&9=xaycht0folcukjibm75nbws6s6$dgj4+d'
 
-SECRET_KEY = shop_secret.sec
+SECRET_KEY = Secret_file.sec
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
