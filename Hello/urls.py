8e729cc4688fbd227cf8d8cf02745626b0ca6b8e
gqadirdepar <gqadirdepar@gmail.com>
2021-01-14 16:46:29 +0500

    updated webpages
    updated pages

--- Hello/urls.py
+++ Hello/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path , include
 
+admin.site.site_header = "Allah Tawakal Admin"
+admin.site.site_title = "Admin Portal"
+admin.site.index_title = "Welcome to Admin Portal"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
